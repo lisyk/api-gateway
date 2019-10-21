090f79d69d43c60e1d2ff17289f6be2a84b150a1
jlblau <jblau@bitwerx.com>
2019-10-21 16:16:02 -0400

    commit to pull tylers fixes

--- components/wellness/config/routes.rb
+++ components/wellness/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Wellness::Engine.routes.draw do
-mount Wellness::Engine::OpenApi::Rswag::Ui::Engine => '/api-docs'
- # mount OpenApi::Rswag::Api::Engine => '/wellness/api-docs'
+  mount OpenApi::Rswag::Api::Engine => '/api-docs'
+  mount OpenApi::Rswag::Ui::Engine => '/api-docs'
   get 'plans/index'
 end
--- components/wellness/lib/wellness/engine.rb
+++ components/wellness/lib/wellness/engine.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
+require 'rubygems'
+require 'open_api-rswag-api'
+require 'open_api-rswag-ui'
+require 'open_api-rswag-specs'
+require 'rspec-rails'
 module Wellness
   class Engine < ::Rails::Engine
-   # isolate_namespace Wellness
+    isolate_namespace Wellness
     config.generators.api_only = true
     config.generators do |g|
       g.test_framework :rspec
