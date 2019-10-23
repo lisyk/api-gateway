d992136ff97e9b1a3a4b5e9a176f93da2b02053b
jlblau <jblau@bitwerx.com>
2019-10-23 12:36:09 -0400

    setup of swagger in APIGateway

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount OpenApi::Rswag::Ui::Engine => '/vip-api-docs'
+  mount OpenApi::Rswag::Api::Engine => '/vip-api-docs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Wellness::Engine, at: '/wellness', as: 'wellness'
   namespace :api do
