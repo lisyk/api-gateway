34ca7d60de9669d827d78e872f459f618682b70d
Tyler Porter <tporter@bitwerx.com>
2019-10-23 16:40:04 -0400

    Added PUT endpoint for agreements

--- components/wellness/app/controllers/wellness/agreements_controller.rb
+++ components/wellness/app/controllers/wellness/agreements_controller.rb
@@ -13,12 +13,11 @@ module Wellness
     end
 
     def update
-      @response ||= demo_client_ready ? client_request(agreement_params) : test_agreement_upload
-      status = @response.status
-      if status == 200
-        render json: { success: ['Signed agreement posted successfully'] }, status: status
+      response = client_request(agreement_params)
+      if response.status == 200
+        render json: { success: ['Signed agreement posted successfully'] }, status: :success
       else
-        render json: { errors: [@response.reason_phrase] }, status: status
+        render json: { errors: [response.reason_phrase] }, status: response.status
       end
     end
 
