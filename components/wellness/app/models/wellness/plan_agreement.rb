019965216ba0097f202c12df7ce436dc298f153f
Sergii Liskovyi <sergey.liskovoy@gmail.com>
2019-10-25 22:40:11 -0400

    Refs #167909379. namespaced properly

---
+++ components/wellness/app/models/wellness/plan_agreement.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+# this model is responsible for Agreements
+# no other services handled by this model
+
+module Wellness
+  class PlanAgreement < Connect
+    def api_request(controller, action, params = {})
+      method_name = request_method(controller, action)
+      resource_name = request_resource(controller, action, params)
+      response = api_client.send(method_name, resource_name)
+      parse_response(response) if response
+    end
+
+    private
+
+    def parse_response(response)
+      if response.headers['content-type'].include?('application/json')
+        response = JSON.parse(response.body)
+      end
+      response
+    end
+
+    def request_mapper(controller, action)
+      mapper = YAML.safe_load(
+          File.read(
+              File.expand_path('config/client_endpoints/endpoints.yml', Wellness::Engine.root)
+          )
+      )
+      mapper[controller][action]
+    end
+
+    def request_resource(controller, action, params)
+      resource = request_mapper(controller, action)['resource']
+      resource += "/#{params[:id]}" if params[:id].present?
+      resource
+    end
+
+    def request_method(controller, action)
+      request_mapper(controller, action)['method']
+    end
+  end
+end
