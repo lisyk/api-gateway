0542d445688c99df5b9afbaa3bd9d853fb4072b9
Tyler Porter <tporter@bitwerx.com>
2019-10-29 15:12:09 -0400

    Include rswag specs for plan services

---
+++ spec/integration/wellness/plan_services_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'swagger_helper'
+require './app/controllers/api/v1/authentication_controller.rb'
+
+describe 'Wellness Plans API', swagger_doc: 'wellness/v1/swagger.json' do
+  path '/wellness/plans/plan_services' do
+    get 'Index' do
+      tags 'Plan Services'
+      produces 'application/json'
+      consumes 'application/json'
+      security [bearer_auth: []]
+
+      context 'Using valid credentials' do
+        let(:token) do
+          post '/api/v1/authentication', params: { user_name: 'test', password: 'test' }
+          JSON.parse(response.body)['token']
+        end
+
+        response '200', 'Retrieve list of plan services' do
+          schema '$service' => '#/definitions/service'
+          let(:Authorization) { " Authorization: Bearer #{token} " }
+          let(:plans) {}
+          run_test!
+        end
+      end
+    end
+  end
+end
