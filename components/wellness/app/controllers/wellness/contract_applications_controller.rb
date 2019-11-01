12716deb84dfecd136b0f04fa8581a9569955fb0
Tyler Porter <tporter@bitwerx.com>
2019-11-01 11:37:28 -0400

    Fix code lint error

--- components/wellness/app/controllers/wellness/contract_applications_controller.rb
+++ components/wellness/app/controllers/wellness/contract_applications_controller.rb
@@ -3,7 +3,7 @@
 module Wellness
   class ContractApplicationsController < Wellness::ApplicationController
     before_action :user_authorized?
-    
+
     def index
       # TODO: needs to be updated from DEMO to PROD
       @applications ||= contract_apps if demo_client_ready
