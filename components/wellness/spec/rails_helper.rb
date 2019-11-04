d2f7249fe10de9560901cf87b533e2d970c6705a
jlblau <jblau@bitwerx.com>
2019-11-04 12:25:47 -0500

    fix code lint

--- components/wellness/spec/rails_helper.rb
+++ components/wellness/spec/rails_helper.rb
@@ -3,7 +3,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 
 system('cp ../../.env.test .')
-#system('cp ../../.env.test.local . || :')
+system('cp ../../.env.test.local . || :')
 
 require File.expand_path('dummy/config/environment', __dir__)
 # This file is copied to spec/ when you run 'rails generate rspec:install'
