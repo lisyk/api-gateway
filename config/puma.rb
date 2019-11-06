088ab9ee0be76d80a41f5ce92f5d90b90d9d83c9
Jesse House <mail@jessehouse.com>
2019-11-06 13:24:37 -0800

    [#169465267] fix lint violation Style/FrozenStringLiteralComment

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#config
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
