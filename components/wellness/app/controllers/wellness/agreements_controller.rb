019481f505c033d91f22924019f7a62dcbdcdc3d
Tyler Porter <tporter@bitwerx.com>
2019-11-07 13:22:43 -0500

    Add rspec tests for agreements controller

--- components/wellness/app/controllers/wellness/agreements_controller.rb
+++ components/wellness/app/controllers/wellness/agreements_controller.rb
@@ -13,10 +13,10 @@ module Wellness
     end
 
     def update
+      # TODO: needs to be updated from DEMO to PROD
       @response ||= put_agreement(agreement_params) if demo_client_ready
       if @response.present?
-        render json: { success: ['Signed agreement posted successfully.'] },
-               status: :success
+        render json: { success: ['Signed agreement posted successfully.'] }
       else
         render json: { errors: ['Agreement could not be uploaded.'] },
                status: :unprocessable_entity
