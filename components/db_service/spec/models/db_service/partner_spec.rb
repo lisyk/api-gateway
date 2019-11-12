319243e77f781f22dd2ba8d6e3321f74f3497c45
Sergii Liskovyi <sergey.liskovoy@gmail.com>
2019-11-11 21:56:56 -0500

    Refs #169490116. specs refactor, add for endpoints

--- components/db_service/spec/models/db_service/partner_spec.rb
+++ components/db_service/spec/models/db_service/partner_spec.rb
@@ -1,40 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe DbService::Partner, type: :model do
-  let(:valid_partner) { DbService::Partner.new(name: 'vcp', root_domain: 'vcp.vet') }
-  let(:partner_no_domain) { DbService::Partner.new(name: 'vcp') }
-  let(:partner_no_name) { DbService::Partner.new(root_domain: 'vcp.vet') }
-  context 'presence' do
-    it 'is valid with partner name and root_domain' do
-      expect(valid_partner).to be_valid
-    end
-    it 'is not valid without root_domain' do
-      expect(partner_no_domain).not_to be_valid
-      expect(partner_no_domain.errors[:root_domain]).to include "can't be blank"
-    end
-    it 'is not valid without name' do
-      expect(partner_no_name).not_to be_valid
-      expect(partner_no_name.errors[:name]).to include "can't be blank"
-    end
+  describe 'database columns' do
+    it { should have_db_column(:name).of_type(:string) }
+    it { should have_db_column(:root_domain).of_type(:string) }
   end
 
-  context 'uniqueness' do
-    let(:name_exists) { DbService::Partner.new(name: 'vcp', root_domain: 'wellness.com') }
-    let(:domain_exists) { DbService::Partner.new(name: 'wellness_partner', root_domain: 'vcp.vet') }
-    let(:all_uniq) { DbService::Partner.new(name: 'wellness_partner', root_domain: 'wellness.com') }
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:root_domain) }
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:root_domain) }
+  end
 
-    it 'is valid with uniq partner name and domain' do
-      expect(all_uniq).to be_valid
-    end
-    it 'is not valid with duplicate partner name' do
-      valid_partner.save!
-      name_exists.valid?
-      expect(name_exists.errors[:name]).to include "has already been taken"
-    end
-    it 'is not valid with duplicate root_domain' do
-      valid_partner.save!
-      name_exists.valid?
-      expect(name_exists.errors[:name]).to include "has already been taken"
-    end
+  describe 'associations' do
+    it { should have_many(:endpoints) }
   end
 end
