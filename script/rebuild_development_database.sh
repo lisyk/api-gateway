5b1128cd101b6ae922629ac6154c747e60a2d525
Jesse House <mail@jessehouse.com>
2019-11-21 16:53:30 -0800

    [#170610188] add base_schema

--- script/rebuild_development_database.sh
+++ script/rebuild_development_database.sh
@@ -3,6 +3,8 @@ set -e # exit on error
 echo 'BEGIN'
 bundle exec rails db:drop
 bundle exec rails db:create
+cp db/base_schema.rb db/schema.rb
+bundle exec rails db:schema:load
 bundle exec rails railties:install:migrations
 bundle exec rails db_service:install:migrations
 bundle exec rails db:migrate
