b17fdb8cc3a1e69012400bbc06bad021df5dc733
Tyler Porter <tporter@bitwerx.com>
2019-12-02 14:41:05 -0500

    Refactor plan constructor to services for Rubocop length limit

--- components/wellness/app/models/wellness/constructors/plan_constructor.rb
+++ components/wellness/app/models/wellness/constructors/plan_constructor.rb
@@ -3,6 +3,8 @@
 module Wellness
   module Constructors
     class PlanConstructor < ResponseLogger
+      include Wellness::Services::PlanFilterService
+
       attr_reader :plans, :constructor_mapper
 
       def initialize(plans, constructor_mapper, params)
@@ -12,15 +14,6 @@ module Wellness
         @is_sellable = params[:is_sellable]
         @species = params[:species]
         @age = params[:age]
-      end
-
-      def filter_plan?(plan)
-        [
-          filter_clinic_location(plan),
-          filter_sellable(plan),
-          filter_species(plan),
-          filter_age(plan)
-        ].any?(true)
       end
 
       def modify
@@ -33,7 +26,8 @@ module Wellness
         else
           update_plan(plans)
         end
-        plans.compact.empty? ? { message: ['No plans matched query'] } : plans.compact
+
+        output_results
       end
 
       def update_plan(plan)
@@ -50,86 +44,11 @@ module Wellness
         plan
       end
 
-      def filter_clinic_location(plan)
-        return false if @clinic_location_id.nil?
-
-        return true if plan['location']['externalLocationCd'] != @clinic_location_id
-
-        false
-      end
-
-      def filter_sellable(plan)
-        return false if @is_sellable.nil?
-
-        is_sellable = @is_sellable.downcase == 'true'
-
-        if is_sellable
-          !plan_active?(plan) || expired_or_early?(plan)
-        else
-          plan_active?(plan) && !expired_or_early?(plan)
-        end
-      end
-
-      def filter_species(plan)
-        return false if @species.nil?
-
-        species_codes = @species.split(',')
-        species_codes.each do |code|
-          return false if plan['species'].present? && plan['species'] == code.to_i
-        end
-        true
-      end
-
-      def filter_age(plan)
-        return false if @age.nil?
-
-        age_in_years = convert_age
-        group = age_groups.select { |_k, v| v <= age_in_years }.max.first.to_i
-
-        return true if plan['ageGroup'].present? && plan['ageGroup'] % 10 != group
-
-        false
+      def output_results
+        plans.compact.empty? ? { message: ['No plans matched query'] } : plans.compact
       end
 
       private
-
-      def expired_or_early?(plan)
-        expired = DateTime.current > plan['planExpirationDate'].to_datetime
-        early = DateTime.current < plan['planEffectiveDate'].to_datetime
-
-        expired || early
-      end
-
-      def plan_active?(plan)
-        plan['planStatus'] == 'A'
-      end
-
-      def age_groups
-        # TODO: Determine actual age groups
-        {
-          '1' => 0,
-          '2' => 1,
-          '3' => 3,
-          '4' => 7
-        }
-      end
-
-      def convert_age
-        return @age if @age.is_a? Numeric
-
-        if @age.include?(':')
-          ((DateTime.current - @age.to_datetime) / 364).floor
-        else
-          parse_age_string
-        end
-      end
-
-      def parse_age_string
-        age_years = @age.scan(/\d+(?=[Yy])/).first.to_i || 0
-        age_months = @age.scan(/\d+(?=[Mm])/).first.to_i || 0
-
-        [@age.to_i, age_years + (age_months / 12).floor].max
-      end
 
       def ignore_field?(key)
         %w[
@@ -141,6 +60,15 @@ module Wellness
           planExpirationDate
           planStatus
         ].include?(key)
+      end
+
+      def filter_plan?(plan)
+        [
+          filter_clinic_location(plan),
+          filter_sellable(plan),
+          filter_species(plan),
+          filter_age(plan)
+        ].any?(true)
       end
     end
   end
