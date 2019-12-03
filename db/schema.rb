63adff9e16ba7fbd849b8bc86f1bf314d4d29d0d
Sergii Liskovyi <sergey.liskovoy@gmail.com>
2019-12-03 16:11:26 -0500

    Refs #169893780. vip_fields drop table. delete migrations. update schema

--- db/schema.rb
+++ db/schema.rb
@@ -57,11 +57,4 @@ ActiveRecord::Schema.define(version: 2019_11_26_024847) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "db_service_vip_fields", force: :cascade do |t|
-    t.string "field_name"
-    t.string "field_data_type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
