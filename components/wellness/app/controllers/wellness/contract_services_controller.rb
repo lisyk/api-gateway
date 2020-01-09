57f724a3ef7cf3f0efb7676a405d3e568d00a150
Sergii Liskovyi <sergey.liskovoy@gmail.com>
2020-01-09 08:28:02 -0500

    Refs #169095444. consume contract service endpoint, translation

--- components/wellness/app/controllers/wellness/contract_services_controller.rb
+++ components/wellness/app/controllers/wellness/contract_services_controller.rb
@@ -13,6 +13,21 @@ module Wellness
       end
     end
 
+    def create
+      translated_request = translate(request, true)
+      @response ||= consume_service(translated_request)
+      if @response.is_a?(Hash) && @response.keys == ['errors']
+        render json: @response,
+               status: :bad_request
+      elsif @response.present?
+        render json: @response
+      else
+        render json: { errors: ['Service consumption error.'] },
+               status: :unprocessable_entity
+      end
+
+    end
+
     private
 
     def fetch_services
@@ -22,6 +37,15 @@ module Wellness
 
     def query_params
       params.except(:format).permit(:contractId)
+    end
+
+    def consume_service(request)
+      contract_service = ContractService.new(controller_name, action_name)
+      contract_service.api_post(request)
+    end
+
+    def translate(request, skip_default)
+      RequestTranslation.new(request, controller_name, skip_default).translate_request.to_json
     end
   end
 end
