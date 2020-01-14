3dda63ad5be7e2162e6f621ca00e16fae36f2850
Tyler Porter <tporter@bitwerx.com>
2020-01-14 13:48:18 -0500

    Add useful messages to job

--- components/wellness/app/jobs/wellness/verify_s3_bucket_job.rb
+++ components/wellness/app/jobs/wellness/verify_s3_bucket_job.rb
@@ -3,16 +3,19 @@
 module Wellness
   class VerifyS3BucketJob < ApplicationJob
     include Services::AwsS3Service
+    include Services::S3BucketJobMessageService
 
     queue_as :default
 
     def perform(*_args)
+      start_job
       response = connection.client.send(:get, 'contract')
-      parsed_response = JSON.parse(response.body)
-      errors = verify_s3_contents(parsed_response)
-      return unless errors.any?
-
-      reconcile_agreements(errors)
+      new_contracts = JSON.parse(response.body)
+      @status = status_template
+      errors = verify_s3_contents(new_contracts)
+      reconcile_agreements(errors) if errors.any?
+      finish_job
+      display_results
     end
 
     private
@@ -21,20 +24,29 @@ module Wellness
       Wellness::Connect.new
     end
 
-    def verify_s3_contents(response)
-      response.map do |contract|
-        id = contract['id'].to_s if contract['id'].present?
-        next if id.nil? || s3_objects.any? { |file| file.key.match?(id) }
+    def status_template
+      { count: new_contracts.size, nil_id: 0, dl_failed: [], ul_failed: [], found: [] }
+    end
 
-        { id: id, message: "Agreement with ID #{id} not present in S3 bucket." }
+    def verify_s3_contents(contracts)
+      contracts.map do |contract|
+        id = contract['id'].to_s if contract['id'].present?
+        if id && s3_objects.any? { |file| file.key.match?(id) }
+          @status[:found] << id
+          next
+        end
+
+        @status[:nil_id] += 1 if id.nil?
+        id
       end.compact
     end
 
-    def reconcile_agreements(errors)
-      errors.each do |error|
-        agreement_id = error[:id]
-        document = download_document(agreement_id)
-        upload_document(agreement_id, document)
+    def reconcile_agreements(error_ids)
+      error_ids.each do |id|
+        document = download_document(id)
+        next if document.nil?
+
+        upload_document(id, document)
       end
     end
 
@@ -43,12 +55,27 @@ module Wellness
     end
 
     def download_document(id)
+      print "#{DateTime.current}: Downloading agreement from partner for ID##{id}... "
       response = connection.client.send(:get, "contractApplicationAgreement/#{id}")
-      Base64.strict_encode64(response.body)
+      if response.status == 200
+        puts 'Success'
+        return Base64.strict_encode64(response.body)
+      else
+        puts 'Failed'
+        message[:dl_failed] << id
+      end
+      nil
     end
 
     def upload_document(agreement_id, document)
-      s3_bucket.object("#{agreement_id}.pdf").put(body: document)
+      print "#{DateTime.current}: Uploading agreement to S3 for ID##{id}... "
+      upload = s3_bucket.object("#{agreement_id}.pdf").put(body: document)
+      if upload.etag.present?
+        puts 'Success'
+      else
+        puts 'Failed'
+        message[:ul_failed] << id
+      end
     end
   end
 end
